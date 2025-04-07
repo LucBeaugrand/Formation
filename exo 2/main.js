63a46ee615555e7eae685a9aa98300aea08574fe
LucBeaugrand <luc.beaugrand62@gmail.com>
2025-04-07 14:32:12 +0200

    beautify exo 2

--- exo 2/main.js
+++ exo 2/main.js
@@ -2,14 +2,14 @@
 function createLogger(prefix) {
     return (msg) => `${prefix} ${msg}`
 }
-  
+
 const logInfo = createLogger("[INFO]");
 console.log(logInfo("Connexion établie")); // Résultat attendu : "[INFO] Connexion établie"
 
 // exercice 2
 function createMultiplier(n) {
-    return (m) => n*m
+    return (m) => n * m
 }
-  
+
 const triple = createMultiplier(3);
 console.log(triple(4)); // Résultat attendu : 12
