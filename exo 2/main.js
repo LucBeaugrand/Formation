518187f4ad3d7b0e3d95f77e2ec9ffbccb5b443a
LucBeaugrand <luc.beaugrand62@gmail.com>
2025-04-07 14:28:59 +0200

    exo 2

---
+++ exo 2/main.js
@@ -0,0 +1,15 @@
+// exercice 1
+function createLogger(prefix) {
+    return (msg) => prefix + " " + msg
+}
+  
+const logInfo = createLogger("[INFO]");
+console.log(logInfo("Connexion établie")); // Résultat attendu : "[INFO] Connexion établie"
+
+// exercice 2
+function createMultiplier(n) {
+    return (m) => n*m
+  }
+  
+const triple = createMultiplier(3);
+console.log(triple(4)); // Résultat attendu : 12
