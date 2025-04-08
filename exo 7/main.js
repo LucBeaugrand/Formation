05b2860d0dd6079df77da465ea2ce1a7a243ff1a
LucBeaugrand <luc.beaugrand62@gmail.com>
2025-04-08 16:43:49 +0200

    cowsay

--- exo 7/main.js
+++ exo 7/main.js
@@ -1,4 +1,7 @@
 import { capitalize,reverse } from "./springTools.js";
+import * as cowsay from "cowsay"
+
+console.log(cowsay.say({text: `Hey mais je suis une vache`}))
 
 console.log(capitalize("luc"))
 console.log(reverse("luc"))
